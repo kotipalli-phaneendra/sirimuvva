19fe1cc2561fa39c416f604cb560a33c454cb718
kotipalli-phaneendra <kphaneendra337@gmail.com>
2024-06-14 02:08:00 +0530

    6-14 workupdate

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.getElementById("navbar");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      // Adjust the scroll value as needed
+      navbar.classList.add("navbar-onscroll");
+    } else {
+      navbar.classList.remove("navbar-onscroll");
+    }
+  });
+});
