98d04f7fd07a89ed55cff080e257f5313c65cb6d
kotipalli-phaneendra <kphaneendra337@gmail.com>
2024-06-23 17:38:26 +0530

    Updated the changes

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var navbar = document.getElementById("header");
+    var navbar = document.getElementById("header");
 
-  window.addEventListener("scroll", function () {
-      if (window.scrollY > 50) {
-          navbar.classList.add("navbar-onscroll");
-      } else {
-          navbar.classList.remove("navbar-onscroll");
-      }
-  });
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("navbar-onscroll");
+        } else {
+            navbar.classList.remove("navbar-onscroll");
+        }
+    });
 });
