9baadddf05fac3dc8776c1d2e4dca812935fe710
kotipalli-phaneendra <kphaneendra337@gmail.com>
2024-06-23 15:23:26 +0530

    Onscroll animation and CSS changes

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,11 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    var navbar = document.getElementById("navbar");
-  
-    window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("header");
+
+  window.addEventListener("scroll", function () {
       if (window.scrollY > 50) {
-        // Adjust the scroll value as needed
-        navbar.classList.add("navbar-onscroll");
+          navbar.classList.add("navbar-onscroll");
       } else {
-        navbar.classList.remove("navbar-onscroll");
+          navbar.classList.remove("navbar-onscroll");
       }
-    });
   });
-  
+});
